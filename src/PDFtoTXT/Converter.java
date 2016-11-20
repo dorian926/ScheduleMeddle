4cbc92c15dced0c6cadecdaff08cb42811a075c8
dorian926 <dorian92@live.unc.edu>
2016-11-19 21:23:32 -0500

    added doc support

--- src/PDFtoTXT/Converter.java
+++ src/PDFtoTXT/Converter.java
@@ -1,6 +1,7 @@
 package PDFtoTXT;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,21 +9,44 @@ import java.nio.file.StandardOpenOption;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+
 
 public class Converter {
-
 	public boolean convert(String input) {
-		try {
-			PDFTextStripper stripper = new PDFTextStripper(); //create a new stripper from pdfbox api to strip text from pdf
-			PDDocument document = PDDocument.load(new File(input)); //take the input of the file and have it loaded as PDF Document, pdfbox api
-			System.out.println("Howdy, I picked up the File!"); // nice lil confirmation
-			String s = stripper.getText(document); //strip the text from the document. One whole line including newline symbols, does '\t'?
-			Files.write(Paths.get("syllabus.txt"), s.getBytes(), StandardOpenOption.CREATE); //write the stripped text to a new file called syllabus.txt
-			return true;
-		} catch (IOException e) {
-			System.out.println("failed to get input"); //lol you failed son
-			e.printStackTrace();
+		File fp = new File(input);
+		String s = null;
+		
+		if(input.contains(".doc")) { //word handling
+			try {
+				HWPFDocument wordDoc = new HWPFDocument(new FileInputStream(fp)); //load word doc from input
+				WordExtractor extractor = new WordExtractor(wordDoc); //extractor takes formatted text from doc
+				s = extractor.getText(); // add text to s
+				wordDoc.close(); //close document, done with it
+				extractor.close();//close extractor, done with it
+				Files.write(Paths.get("syllabus.txt"), s.getBytes(), StandardOpenOption.CREATE); //write the stripped text to a new file called syllabus.txt
+				return true;
+			} catch (IOException e) {
+				printException(e);
+			}
+		} else if(input.contains(".pdf")) { //pdf handling
+			try {
+				PDFTextStripper stripper = new PDFTextStripper(); //create a new stripper from pdfbox api to strip text from pdf
+				PDDocument pdfDoc = PDDocument.load(fp); //take the input of the file and have it loaded as PDF Document, pdfbox api
+				s = stripper.getText(pdfDoc); //strip the text from the document. One whole line including newline symbols, does '\t'?
+				pdfDoc.close(); //close document done with it
+				Files.write(Paths.get("syllabus.txt"), s.getBytes(), StandardOpenOption.CREATE); //write the stripped text to a new file called syllabus.txt
+				return true;
+			} catch (IOException e) {
+				printException(e);
+			}
 		}
 		return false;
 	}
+
+public void printException(Exception e) {
+	System.out.println("Failed to retrieve file");
+	System.out.println(e.getMessage());
+}
 }
--- src/PDFtoTXT/Worker.java
+++ src/PDFtoTXT/Worker.java
@@ -4,7 +4,6 @@ public class Worker {
 
 	public static void main(String[] args) {
 		Converter converter = new Converter();
-		System.out.println("printing args[0]");
 		converter.convert(args[0]);
 	}
 
