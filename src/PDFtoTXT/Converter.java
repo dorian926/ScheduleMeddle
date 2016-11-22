e7ede23f56b3b0766674d27bce996e5a53f8c8e5
dorian926 <dorian92@live.unc.edu>
2016-11-22 12:04:45 -0500

    test syllabus.txt file. changed conversion to assist parsing

--- src/PDFtoTXT/Converter.java
+++ src/PDFtoTXT/Converter.java
@@ -13,6 +13,8 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 
 public class Converter {
@@ -29,14 +31,40 @@ public class Converter {
 		}
 		String s = null;
 		
-		if(input.contains(".doc")) { //word handling
+		if(input.contains(".docx")) { //word handling
+			try {
+				XWPFDocument wordXDoc = new XWPFDocument(new FileInputStream(fp));
+				XWPFWordExtractor extractor = new XWPFWordExtractor(wordXDoc); //extractor takes formatted text from doc
+				s = extractor.getText(); // add text to s
+				wordXDoc.close(); //close document, done with it
+				extractor.close();//close extractor, done with it
+				String[] periodSplit = s.split("\\.");
+				for(String line : periodSplit) {
+					line.replace("\n", " ");
+					line.replace("\t", " ");
+					line += "\n";
+					bw.write(line);
+				}
+				bw.close();
+				//Files.write(Paths.get("syllabus.txt"), s.getBytes(), StandardOpenOption.CREATE); //write the stripped text to a new file called syllabus.txt
+				return output;
+			} catch (IOException e) {
+				printException(e);
+			}
+		} else if(input.contains(".doc")) {
 			try {
 				HWPFDocument wordDoc = new HWPFDocument(new FileInputStream(fp)); //load word doc from input
 				WordExtractor extractor = new WordExtractor(wordDoc); //extractor takes formatted text from doc
 				s = extractor.getText(); // add text to s
 				wordDoc.close(); //close document, done with it
 				extractor.close();//close extractor, done with it
-				bw.write(s);
+				String[] periodSplit = s.split("\\.");
+				for(String line : periodSplit) {
+					line.replace("\n", " ");
+					line.replace("\t", " ");
+					line += "\n";
+					bw.write(line);
+				}
 				bw.close();
 				//Files.write(Paths.get("syllabus.txt"), s.getBytes(), StandardOpenOption.CREATE); //write the stripped text to a new file called syllabus.txt
 				return output;
@@ -49,7 +77,13 @@ public class Converter {
 				PDDocument pdfDoc = PDDocument.load(fp); //take the input of the file and have it loaded as PDF Document, pdfbox api
 				s = stripper.getText(pdfDoc); //strip the text from the document. One whole line including newline symbols, does '\t'?
 				pdfDoc.close(); //close document done with it
-				bw.write(s);
+				String[] periodSplit = s.split("\\.");
+				for(String line : periodSplit) {
+					line.replace("\n", " ");
+					line.replace("\t", " ");
+					line += "\n";
+					bw.write(line);
+				}
 				bw.close();
 				//Files.write(Paths.get("syllabus.txt"), s.getBytes(), StandardOpenOption.CREATE); //write the stripped text to a new file called syllabus.txt
 				return output;
